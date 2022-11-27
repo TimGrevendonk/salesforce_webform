623621e7fa44758706cdddb06c685ec450e8a6d6
TimGrevendonk <r0831309@student.thomasmore.be>
2022-11-27 12:30:23 +0100

    where thanks page?

--- angular_salesforce_webform/src/app/webform/webform.component.ts
+++ angular_salesforce_webform/src/app/webform/webform.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-webform',
   templateUrl: './webform.component.html',
-  styleUrls: ['./webform.component.css']
+  styleUrls: ['./webform.component.css'],
 })
 export class WebformComponent implements OnInit {
+  isSubmitted = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.isSubmitted = true;
   }
-
 }
